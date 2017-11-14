09e51f2222784d2526c9a37589ba0026898a0795
Lucas Marino <lucas.marino@etermax.com>
2017-11-14 02:07:37 -0300

    added game over popup and its controller with its references

---
+++ Assets/Scripts/Presentation/UI/GameOverPopup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverPopup : MonoBehaviour
+{
+
+	public Button BuyButton;
+	public Button ContinueButton;
+
+	private void Awake()
+	{
+		
+	}
+
+}
