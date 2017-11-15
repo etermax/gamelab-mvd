12f46d7ea173a3f178d6d96a4a3c457fcf0685fc
Lucas Marino <lucas.marino@etermax.com>
2017-11-15 16:13:02 -0300

    hurt enemy test

---
+++ Assets/Scripts/Editor/Tests/Core/Domain/Actions/HurtEnemyTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Core.Domain.Actions
+{
+    [TestFixture, Category("Actions")]
+    public class HurtEnemyTest
+    {
+        private HurtEnemy hurtEnemy;
+        private Enemy enemy;
+
+        [Test]
+        public void EnemyIsHurtAndItDies()
+        {
+            GivenAHurtEnemy();
+            GivenAnEnemyWithLastLife();
+            WhenEnemyIsHurt();
+            ThenEnemyDies();
+        }
+
+        private void ThenEnemyDies()
+        {
+            Assert.IsTrue(enemy.IsDeath());
+        }
+
+        private void WhenEnemyIsHurt()
+        {
+            hurtEnemy.Execute(enemy);
+        }
+
+        private void GivenAnEnemyWithLastLife()
+        {
+            enemy = new Enemy();
+            enemy.Life = 1;
+        }
+
+        private void GivenAHurtEnemy()
+        {
+            hurtEnemy = new HurtEnemy();
+        }
+    }
+}
