876ed47817dc3b4576ac41dfedf132ea146d024f
Jorge Nicolás Nogueiras <nogueirasjorge@gmail.com>
2017-11-15 16:08:22 -0300

    new Test for CanLayBomb action

---
+++ Assets/Scripts/Editor/Tests/Core/Domain/Actions/CanLayBombTest.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using Presentation.Game;
+
+namespace Core.Domain.Actions
+{
+    [TestFixture, Category("Actions")]
+    public class CanLayBombTest
+    {
+        CanLayBomb action;
+        PlayerStats prefs;
+        bool canLayBomb;
+
+        [Test]
+        public void UserCanLayBombWhenHasBombsAndNoBombHasBeenLayed()
+        {
+            GivenACanLayBomb();
+            WhenUserHasBombsAndNoBombsHasBeenLayed();
+            ThenBombCanBeLayed();
+        }
+
+        [Test]
+        public void UserCanNotLayABombWhenHasBombsAndABombHasBeenLayed()
+        {
+            GivenACanLayBomb();
+            WhenUserHasBombsAndABombHasBeenLayed();
+            ThenBombCanNotBeLayed();
+        }
+
+        [Test]
+        public void UserCanNotLayABombWhenDoesNotHaveOne()
+        {
+            GivenACanLayBomb();
+            WhenUserDoesNotHaveABomb();
+            ThenBombCanNotBeLayed();
+        }
+
+        private void WhenUserDoesNotHaveABomb()
+        {
+            prefs = new PlayerStats
+            {
+                BombLayed = false,
+                Bombs = 0
+            };
+            canLayBomb = action.Execute(prefs);
+        }
+
+        private void ThenBombCanNotBeLayed()
+        {
+            Assert.IsFalse(canLayBomb);
+        }
+
+        private void WhenUserHasBombsAndABombHasBeenLayed()
+        {
+            prefs = new PlayerStats
+            {
+                BombLayed = true,
+                Bombs = 1
+            };
+            canLayBomb = action.Execute(prefs);
+        }
+
+        private void ThenBombCanBeLayed()
+        {
+            Assert.IsTrue(canLayBomb);
+        }
+
+        private void WhenUserHasBombsAndNoBombsHasBeenLayed()
+        {
+            prefs = new PlayerStats
+            {
+                BombLayed = false,
+                Bombs = 1
+            };
+            canLayBomb = action.Execute(prefs);
+        }
+
+        private void GivenACanLayBomb()
+        {
+            action = new CanLayBomb();
+        }
+    }
+}
