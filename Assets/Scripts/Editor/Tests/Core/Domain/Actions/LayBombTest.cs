1cae5e417a6a4ea5593145b97a41e126451b9b5a
Jorge Nicolás Nogueiras <nogueirasjorge@gmail.com>
2017-11-15 16:40:29 -0300

    LayBomb Test

---
+++ Assets/Scripts/Editor/Tests/Core/Domain/Actions/LayBombTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Presentation.Game;
+
+namespace Core.Domain.Actions
+{
+    [TestFixture, Category("Actions")]
+    public class LayBombTest
+    {
+        [Test]
+        public void Test()
+        {
+            var action = GivenALayBombAction();
+            var stats = WhenLayBombExecutes(action);
+            ThenBombIsLayed(stats);
+        }
+
+        private static void ThenBombIsLayed(PlayerStats stats)
+        {
+            Assert.IsTrue(stats.BombLayed);
+            Assert.AreEqual(stats.Bombs, 0);
+        }
+
+        private static PlayerStats WhenLayBombExecutes(LayBomb action)
+        {
+            var stats = new PlayerStats
+            {
+                Bombs = 1,
+                BombLayed = false
+            };
+            action.Execute(stats);
+            return stats;
+        }
+
+        private static LayBomb GivenALayBombAction()
+        {
+            var action = new LayBomb();
+            return action;
+        }
+    }
+}
